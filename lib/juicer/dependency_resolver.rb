70b41ff65e6f11dbb04315bbd6e851cf7c01f088
Christian Johansen <christian@shortcut.no>
2009-11-04 08:15:32 +0100

    Refactor Juicer::CSS into modules

---
+++ lib/juicer/dependency_resolver.rb
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+
+require "juicer/io_proxy"
+require "fileutils"
+
+module Juicer
+  #
+  # Provides dependency resolution for linked files. To include the module in a
+  # class, the class needs to provide an io accessor which should return an
+  # object with an <tt>open</tt> method that yields an IO stream.
+  #
+  # = Overview
+  #
+  # Given an class <tt>Resource</tt> (and instance <tt>resource</tt>) that
+  # includes the module, these are the methods provided.
+  #
+  # Depend on another resource.
+  #   resource.depend(Resource.new("myfile"))
+  #
+  # You can depend on the file directly, Juicer will wrap it in an instance of
+  # the including class (e.g. <tt>Resource</tt> in this example) for you
+  #   resource.depend("myfile.txt")
+  #
+  # <tt>&lt;&lt;</tt> is an alias for depend
+  #   resource << "myfile.txt"
+  #
+  # List all dependencies
+  #   resource.dependencies #=> [#<Resource:"myfile.txt">]
+  #
+  # List all resources. This includes self in the list:
+  #   resource.resources #=> [#<Juicer::Resource:[unsaved]>, #<Juicer::Resource:"myfile.txt">]
+  #
+  # Export the resource to a file. Will include dependency statements for any
+  # dependencies
+  #   file = File.open("myfile.txt", "w")
+  #   resource.export(file)
+  #   file.close
+  #
+  # If you'd rather include the contents of the dependencies inline you can
+  # specify the <tt>:inline_dependencies = true</tt> option. In this case, the
+  # dependency statements in the original source are removed.
+  #   resource.export(file, :inline_dependencies => true)
+  #
+  # There are a few alternative ways to export contents:
+  #
+  # Write to open file handler
+  #   File.open("myfile.txt", "w")
+  #   resource.export(file)
+  #   file.close
+  #
+  # Export to filename, analogous to above example
+  #   resource.export("myfile.txt")
+  #
+  # Export in <tt>File.open</tt> block
+  #   File.open("myfile.txt", "w") { |f| resource.export(f) }
+  #
+  # Read contents from resource
+  #   File.open("myfile.txt", "w") { |f| f.write(resource.read) }
+  #
+  # <tt>concat</tt> is an alias to <tt>read(:inline_dependencies => true, :recursive => true)</tt>
+  #   File.open("myfile.txt", "w") { |f| f.write(resource.concat) }
+  #
+  # Of course, any IO stream is acceptable
+  #   resource.export(StringIO.new)
+  #
+  # Wrap an existing CSS resource in a <tt>Juicer::CSS</tt> instance
+  #   css = Juicer::CSS.new("myfile.css")
+  #   css.dependencies # Lists all @import'ed files (recursively) as Juicer::CSS objects
+  #
+  # Author::    Christian Johansen (christian@cjohansen.no)
+  # Copyright:: Copyright (c) 2009 Christian Johansen
+  # License::   BSD
+  #
+  module DependencyResolver
+
+    #
+    # Lists all dependencies, either added in through the depend method, or
+    # through dependency statements in the source content. The method accepts an
+    # options hash which can specify the <tt>:recursive</tt> option. If
+    # <tt>true</tt>, all nested dependencies will be load. The default value is
+    # <tt>false</tt>, producing a list of files directly depended on by the
+    # resource. Dependencies are returned as an array of instances of the class
+    # including the module. Files are resolved through <tt>Juicer::IOProxy</tt>,
+    # meaning they can exist anywhere on <tt>Juicer.load_path</tt>, not
+    # necessarily in the current directory.
+    #
+    def dependencies(options = {})
+      options = { :recursive => false }.merge(options)
+      @_deps = []
+      @_ios = []
+      content_dependencies(io, options[:recursive]).concat(@dependencies || [])
+    end
+
+    #
+    # Lists all instances that make up this resource, including this instance.
+    # The options hash is passed to #dependencies, refer to its documentation
+    # for possible options.
+    #
+    def resources(options = {})
+      dependencies(options) + [self]
+    end
+
+    #
+    # Reads the contents of the resource. By default only the content of the
+    # resource is read out. The <tt>:inline_dependencies</tt> option can be
+    # provided to concatenate all dependencies and produce the full listing. In
+    # this case any dependency statements are removed from the resulting string, to
+    # avoid loading/depending on dependencies twice.
+    #
+    def read(options = {})
+      contents = ""
+
+      if options[:inline_dependencies]
+        dependencies(options).each { |dep| contents << dep.read }
+      end
+
+      contents << io.open { |stream| stream.rewind && stream.read }
+    end
+
+    #
+    # Export the contents to an output. The output is wrapped in a
+    # <tt>Juicer::IOProxy</tt> object, meaning you can provide it any one of the
+    # inputs <tt>Juicer::IOProxy.new</tt> accepts. The options hash is passed to
+    # #read, refer to its documentation for possible options to provide. If a
+    # string is passed to #export, it is used as a file name (i.e., if you want
+    # to write to a string, pass a StringIO instance).
+    #
+    def export(stream_like, options = {})
+      if stream_like.is_a?(String)
+        FileUtils.touch(stream_like)
+      end
+
+      Juicer::IOProxy.open(stream_like, "w+") { |ios| ios.write(read(options)) }
+    rescue ArgumentError => err
+      raise ArgumentError.new("Invalid stream argument, #{stream_like}: #{err.message}")
+    end
+
+    #
+    # Concatenates the resource with all dependencies and returns a new instance
+    # with the full listing as its contents. Equivalent to calling
+    # <tt>Resource.new(read(:inline_dependencies => true, :recursive => true))</tt>
+    # In contrast to <tt>dependencies</tt>, <tt>concat</tt> defaults to
+    # <tt>:recursive => true</tt>, i.e., include all nested dependencies, not
+    # just direct ones.
+    #
+    def concat(options = {})
+      read(options.merge(:inline_dependencies => true, :recursive => true))
+    end
+
+    #
+    # Add a dependency. Accepts instances of the including class,
+    # <tt>Juicer::IOProxy</tt> instances, or any other input accepted by
+    # <tt>Juicer::IOProxy.new</tt>, i.e., file names, string content or io
+    # streams. If the including class implements an <tt>open</tt> class method,
+    # it is used to resolve the resource to add. Otherwise, <tt>#new</tt> is
+    # called directly.
+    #
+    def depend(resource)
+      klass = self.class
+      @dependencies ||= []
+      @dependencies.push(klass.respond_to?(:open) ? klass.open(resource) : klass.new(resource))
+    end
+
+    alias << depend
+
+    #
+    # Two instances are equal if they share the same <tt>Juicer::IOProxy</tt>
+    # object as well as all dependencies
+    #
+    def ==(other)
+      return false if io != other.io
+      dependencies == other.dependencies
+    end
+
+    private
+    def content_dependencies(source, recursive)
+      source.open do |stream|
+        stream.rewind
+
+        while !stream.eof?
+          line = stream.readline
+
+          begin
+            matches = /^\s*@import(?:\s+url\(|\s+)?(['"]?)([^\?'"\)\s]+)(\?[^'"\)]*)?\1\)?(?:[^?;]*);?/im.match(line)
+
+            if matches
+              io = Juicer::IOProxy.load(matches[2])
+              content_dependencies(io, recursive) if !@_ios.include?(io) && recursive
+
+              if !@_ios.include?(io)
+                @_ios.push(io)
+                @_deps.push(Juicer::CSS.new(io))
+              end
+            end
+          rescue RegexpError => err
+            log.error "Encountered an error when extracting dependencies from #{line.strip}" +
+                      "This might indicate a syntax error or possibly a bug in Juicer. Please investigate."
+          end
+        end
+      end
+
+      @_deps
+    end
+  end
+end
---
+++ lib/juicer/io_proxy_wrapper.rb
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+require "juicer/io_proxy"
+
+module Juicer
+  #
+  # Stub implementation of a resource backed by a <tt>Juicer::IOProxy</tt>
+  # object.
+  #
+  # Author::    Christian Johansen (christian@cjohansen.no)
+  # Copyright:: Copyright (c) 2009 Christian Johansen
+  # License::   BSD
+  #
+  module IOProxyWrapper
+    attr_reader :io
+
+    def self.included(klass)
+      klass.extend(ClassMethods)
+    end
+
+    #
+    # Creates a new resource. Accepts a wide variety of input options:
+    # * A file name of an existing file
+    # * An io object
+    # * A string of content
+    # * Nothing - creates a new empty resource
+    #
+    # Additionally, you may provide an array with a mix of the above, or several
+    # arguments, also mixing the above. In any case you can provide an options
+    # hash as the last argument
+    #
+    def initialize(*args)
+      args.flatten!
+      @options = {}
+      @dependencies = []
+
+      if args[-1].is_a?(Hash)
+        @options.merge!(args.pop)
+      end
+
+      if args.length > 1
+        @io = Juicer::IOProxy.new
+        self.import(self.class.new(args.shift, @options)) while args.length > 0
+      else
+        @io = Juicer::IOProxy.load(args[0], @options[:load_path])
+      end
+    rescue ArgumentError
+      raise ArgumentError.new(<<-MSG)
+      Argument(s) to #{self.class}#new needs to be one of: Juicer::IOProxy instance,
+      a string, a file name or an IO object.
+      MSG
+    end
+
+    #
+    # Purdy string representation of a resource
+    #
+    def inspect
+      filename = file.nil? ? "[unsaved]" : "\"#{file}\""
+      "#<#{self.class}:#{filename}>"
+    end
+
+    #
+    # Returns the name of the file the resource wraps, if any. If the resource
+    # does not wrap a file (i.e., it's an io stream, or string), the method
+    # returns <tt>nil</tt>.
+    #
+    def file
+      io.file
+    end
+
+    module ClassMethods
+      #
+      # Open an instance. If input is already an instance of the including class,
+      # it is returned directly. Otherwise, the input is passed directly to
+      # <tt>#new</tt>. In either case, a new instance of the including class is
+      # returned.
+      #
+      def open(stream_like)
+        return stream_like if stream_like.is_a?(self)
+        self.new(stream_like)
+      rescue ArgumentError => err
+        raise err
+      end
+    end
+  end
+end
